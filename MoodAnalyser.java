dcff41be20650fac4b9726c8a48304911bb5cbda
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-12-11 21:37:08 +0530

    Added: Handle exception if user provide null or invalid mood

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,27 +1,33 @@
 package com.bridgelab.mood_analyser;
 
 public class MoodAnalyser {
-	  //Declare variable
+	 //Declare variable
     String mood;
     String message;
 
     //Default constructor
-public MoodAnalyser(){
+    public MoodAnalyser(){
 
     }
+
     //Parameterised constructor
-public MoodAnalyser(String message){
+    public MoodAnalyser(String message){
         this.message = message;
     }
 
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mood Analyser Problem");
+    }
+
     //Method that return mood
-    public String analysisMood(String message) {
-        if (message.equalsIgnoreCase("I am in sad Mood")) {
-            mood = "SAD";
+    public String analysisMood() {
+        try {
+            if (message.contains("sad"))
+                return "sad";
+            else
+                return "Happy";
+        } catch(NullPointerException e) {
+            return "Happy";
         }
-        else {
-            mood = "HAPPY";
-        }
-        return mood;
     }
 }
--- MoodAnylyserTest.java
+++ MoodAnylyserTest.java
@@ -6,12 +6,30 @@ import org.junit.jupiter.api.Test;
 
 
 public class MoodAnylyserTest {
-	MoodAnalyser moodAnalyser = new MoodAnalyser();
+	//Create object of MoodAnalyser class
+    MoodAnalyser moodAnalyser = new MoodAnalyser();
+
+    //Test case to check sad mood
     @Test
-    public void testMoodAnalysis(){
-        String message = "I am in sad Mood";
-        String mood = moodAnalyser.analysisMood(message);
-        assertEquals("SAD", mood);
-    
-	}
+    public void givenMood_WhenSad_ShouldReturnSad(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad Mood");
+        String reaction =moodAnalyser.analysisMood();
+        assertEquals("sad",reaction);
+    }
+
+    //Test case to check happy mood
+    @Test
+    public void givenMood_WhenHappy_ShouldReturnHappy(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy Mood");
+        String reaction =moodAnalyser.analysisMood();
+        assertEquals("Happy",reaction );
+    }
+
+    //Test case to check null mood
+    @Test
+    public void givenMood_WhenHappy_TroughException(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String reaction =moodAnalyser.analysisMood();
+        assertEquals("Happy",reaction );
+    }
 }
